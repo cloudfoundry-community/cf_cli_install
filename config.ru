de54a2ccab436e4672a5ded156fd5e556f28331b
Dr Nic Williams <drnicwilliams@gmail.com>
2013-11-26 11:06:02 -0800

    Loading github releases information for CLI

--- config.ru
+++ config.ru
@@ -1,3 +1,9 @@
 require './fetch_cf_cli'
 
-run FetchCfCli.new
+unless github_access_token = ENV['GITHUB_ACCESS_TOKEN']
+  $stderr.puts "Please set environment variable $GITHUB_ACCESS_TOKEN"
+  $stderr.puts "Create new tokens via https://github.com/settings/applications 'Personal Access Tokens' section"
+  exit 1
+end
+
+run FetchCfCli.new(github_access_token)
--- fetch_cf_cli.rb
+++ fetch_cf_cli.rb
@@ -1,7 +1,34 @@
 require 'sinatra/base'
+require 'json'
+require 'httparty'
 
 class FetchCfCli < Sinatra::Base
+  def initialize(*args)
+    super
+    unless @github_access_token = ENV['GITHUB_ACCESS_TOKEN']
+      $stderr.puts "Please set environment variable $GITHUB_ACCESS_TOKEN"
+      $stderr.puts "Create new tokens via https://github.com/settings/applications 'Personal Access Tokens' section"
+      exit 1
+    end
+  end
+
   get '/' do
-    'Hello World from FetchCfCli in separate file!'
+    content_type :json
+    cli_releases
+  end
+
+
+  def cli_releases
+    response = HTTParty.get(cli_releases_uri, cli_releases_headers)
+    # response.body, response.code, response.message, response.headers.inspect
+    response.body
+  end
+
+  def cli_releases_uri
+    'https://api.github.com/repos/cloudfoundry/cli/releases'
+  end
+
+  def cli_releases_headers
+    { headers: { "Authorization" => "token #{@github_access_token}", "User-Agent" => "fetch_cf_cli by Dr Nic Williams" } }
   end
 end
