ad2c6e75dc7b2d3caef03445fbd8056308d8f8e7
Dr Nic Williams <drnicwilliams@gmail.com>
2013-11-26 12:11:57 -0800

    show download links for each OS version

--- config.ru
+++ config.ru
@@ -1,9 +1,3 @@
 require './fetch_cf_cli'
 
-unless github_access_token = ENV['GITHUB_ACCESS_TOKEN']
-  $stderr.puts "Please set environment variable $GITHUB_ACCESS_TOKEN"
-  $stderr.puts "Create new tokens via https://github.com/settings/applications 'Personal Access Tokens' section"
-  exit 1
-end
-
-run FetchCfCli.new(github_access_token)
+run FetchCfCli.new
--- fetch_cf_cli.rb
+++ fetch_cf_cli.rb
@@ -1,5 +1,5 @@
 require 'sinatra/base'
-require 'json'
+require 'json/pure'
 require 'httparty'
 
 class FetchCfCli < Sinatra::Base
@@ -13,15 +13,39 @@ class FetchCfCli < Sinatra::Base
   end
 
   get '/' do
-    content_type :json
-    cli_releases
+    erb :index, format: :html5, layout: false
   end
 
+  # Convert labels like:
+  # ["gcf-darwin-amd64.tgz", "gcf-linux-amd64.tgz", "gcf-windows-386.zip", "gcf-windows-amd64.zip"]
+  # into names like:
+  # ["darwin-amd64", "linux-amd64", "windows-386", "windows-amd64"]
+  # and return them as an Array of { name => asset }
+  def cli_release_asset_names_to_labels
+    cli_release_assets.inject({}) do |result, asset|
+      label = asset["label"]
+      name = if label =~ /cf-(.*)\.(tgz|zip)/
+        $1
+      else
+        label
+      end
+      result[name] = asset
+      result
+    end
+  end
+
+  def cli_release_assets
+    latest_cli_release["assets"]
+  end
+
+  def latest_cli_release
+    cli_releases.first
+  end
 
   def cli_releases
     response = HTTParty.get(cli_releases_uri, cli_releases_headers)
     # response.body, response.code, response.message, response.headers.inspect
-    response.body
+    JSON.parse(response.body)
   end
 
   def cli_releases_uri
